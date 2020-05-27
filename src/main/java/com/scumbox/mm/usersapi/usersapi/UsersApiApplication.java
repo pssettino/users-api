2c680b04606a10f393358fb22d856a8bac4eceec
Pablo Settino <pablo.settino@mercadolibre.com>
2020-05-26 23:32:37 -0300

    test

--- src/main/java/com/scumbox/mm/usersapi/usersapi/UsersApiApplication.java
+++ src/main/java/com/scumbox/mm/usersapi/usersapi/UsersApiApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
