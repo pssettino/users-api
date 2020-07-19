7ea503068496139b15370e2042bd5d284cb1eaa0
Pablo Settino <pablo.settino@mercadolibre.com>
2020-07-19 15:17:16 -0300

    Add lombok

--- src/main/java/com/scumbox/mm/usersapi/usersapi/persistence/domain/User.java
+++ src/main/java/com/scumbox/mm/usersapi/usersapi/persistence/domain/User.java
@@ -1,10 +1,17 @@
 package com.scumbox.mm.usersapi.usersapi.persistence.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
+@Setter
+@Getter
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -18,44 +25,4 @@ public class User {
     private String email;
 
     private Role role;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
