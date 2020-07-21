de78601c7daf5e14334d94ca2ad266e0017b77a7
Pablo Settino <pablo.settino@mercadolibre.com>
2020-07-21 20:36:55 -0300

    change request mapping

--- src/main/java/com/scumbox/mm/usersapi/usersapi/controller/UserController.java
+++ src/main/java/com/scumbox/mm/usersapi/usersapi/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/users")
 public class UserController {
 
     private UserService userService;
