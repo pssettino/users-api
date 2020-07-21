fdbf1a8d6807ef26659ba81f6fff3805c1934818
Pablo Settino <pablo.settino@mercadolibre.com>
2020-07-20 21:48:07 -0300

    add zuul

--- src/main/java/com/scumbox/mm/usersapi/usersapi/UsersApiApplication.java
+++ src/main/java/com/scumbox/mm/usersapi/usersapi/UsersApiApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
