749a06f0c60109bfa0cc77b03ed5853a1e9e844d
Pablo Settino <pablo.settino@mercadolibre.com>
2020-10-12 16:52:03 -0300

    change shift to shifts

--- src/main/java/com/scumbox/mm/usersapi/usersapi/controller/ShiftController.java
+++ src/main/java/com/scumbox/mm/usersapi/usersapi/controller/ShiftController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/shift")
+@RequestMapping("/api/shifts")
 public class ShiftController {
 
     private ShiftService shiftService;
